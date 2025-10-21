bb71c813a72e4a9612032d21838685b66b4439f2
tamaraghosn <tamaraghosn01@gmail.com>
2025-10-21 14:04:32 +0300

    first commit

---
+++ src/main/java/com/tamara/EventTicketingManager/EventTicketingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.tamara.EventTicketingManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventTicketingManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventTicketingManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tamara/EventTicketingManager/EventTicketingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tamara.EventTicketingManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventTicketingManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
