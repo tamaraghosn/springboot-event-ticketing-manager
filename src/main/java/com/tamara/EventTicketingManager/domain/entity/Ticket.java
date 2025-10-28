318d7bb5753c71bdfa9418b78aa58be35226c291
tamaraghosn <tamaraghosn01@gmail.com>
2025-10-28 13:56:02 +0200

    create ticket class

---
+++ src/main/java/com/tamara/EventTicketingManager/domain/entity/Ticket.java
@@ -0,0 +1,54 @@
+package com.tamara.EventTicketingManager.domain.entity;
+
+
+import com.tamara.EventTicketingManager.domain.TicketStatusEnum;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tickets")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Ticket {
+
+
+    @Id
+    @Column(name = "id", nullable = false , updatable = false)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private TicketStatusEnum status;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ticket_type_id")
+    private TicketType ticketType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "purchaser_id")
+    private User purchaser;
+
+    // todo ticketvalidation
+
+    // todo qrcode
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+
+}
