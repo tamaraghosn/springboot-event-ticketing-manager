a104c64ef033d68c88c9251dee69b746936cf1e3
tamaraghosn <tamaraghosn01@gmail.com>
2025-10-29 13:09:46 +0200

    implementing user repo and create UserProvisioningFilter so every authenticated Keycloak user always has a matching record in my postgres db

---
+++ src/main/java/com/tamara/EventTicketingManager/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.tamara.EventTicketingManager.repository;
+
+import com.tamara.EventTicketingManager.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+
+}
