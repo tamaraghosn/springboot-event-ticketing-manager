91417f544301ba1df2904a6301431db25e1518ed
tamaraghosn <tamaraghosn01@gmail.com>
2025-10-29 13:57:50 +0200

    implementing UserProvisioningFilter to check if user user exist in db if not create a user record from the keycloak JWT token received by spring security

--- src/main/java/com/tamara/EventTicketingManager/filter/UserProvisioningFilter.java
+++ src/main/java/com/tamara/EventTicketingManager/filter/UserProvisioningFilter.java
@@ -1,16 +1,21 @@
 package com.tamara.EventTicketingManager.filter;
 
 
+import com.tamara.EventTicketingManager.domain.entity.User;
 import com.tamara.EventTicketingManager.repository.UserRepository;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @Component
 @RequiredArgsConstructor
@@ -25,8 +30,28 @@ public class UserProvisioningFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof Jwt jwt) {
+
+            // extract keycloak id
+
+            UUID keycloadId = UUID.fromString(jwt.getSubject());
+
+            if (!userRepository.existsById(keycloadId)) {
+
+                User newUser = new User();
+                newUser.setId(keycloadId);
+                newUser.setName(jwt.getClaimAsString("preferred_username"));
+                newUser.setEmail(jwt.getClaimAsString("email"));
+                userRepository.save(newUser);
+
+            }
+
+        }
 
 
+        filterChain.doFilter(request, response);
 
     }
 }
