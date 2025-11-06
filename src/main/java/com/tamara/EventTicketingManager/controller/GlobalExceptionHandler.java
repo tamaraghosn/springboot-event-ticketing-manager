e4dd918ffa6a59c4d0553b06679a0bcf8627633f
tamaraghosn <tamaraghosn01@gmail.com>
2025-11-06 12:38:29 +0200

    implement general exception handler

--- src/main/java/com/tamara/EventTicketingManager/controller/GlobalExceptionHandler.java
+++ src/main/java/com/tamara/EventTicketingManager/controller/GlobalExceptionHandler.java
@@ -1,4 +1,27 @@
 package com.tamara.EventTicketingManager.controller;
 
+import com.tamara.EventTicketingManager.domain.dto.ErrorDto;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
+    
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorDto> handleException(Exception ex){
+        log.error("Caught Exception", ex);
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .error("An unknown error occcured")
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
 }
