32274750a8925aeb002dfb020c2c69b31a071a2f
tamaraghosn <tamaraghosn01@gmail.com>
2025-11-06 12:52:15 +0200

    implement MethodArgumentNotvalidException handler to catch all errors generated from request bodies annotated with @valid annotation

--- src/main/java/com/tamara/EventTicketingManager/controller/GlobalExceptionHandler.java
+++ src/main/java/com/tamara/EventTicketingManager/controller/GlobalExceptionHandler.java
@@ -6,13 +6,35 @@ import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.naming.Binding;
+import java.util.List;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleMethodArgumentNotValidException (MethodArgumentNotValidException ex){
+
+        log.error("Caught MethodArgumentNotValidException", ex);
+
+        BindingResult bindingResult = ex.getBindingResult();
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        String errorMessage = fieldErrors.stream().findFirst().map(fieldError -> fieldError.getField() + ": "+ fieldError.getDefaultMessage()).orElse("Validation Error occured");
+
+        ErrorDto errorDto = ErrorDto.builder()
+                .error(errorMessage)
+                .build();
+
+        return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
+    }
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ErrorDto> handleConstraintViolation (ConstraintViolationException ex){
@@ -33,7 +55,7 @@ public class GlobalExceptionHandler {
 
         return new ResponseEntity<>(errorDto, HttpStatus.BAD_REQUEST);
     }
-    
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorDto> handleException(Exception ex){
         log.error("Caught Exception", ex);
