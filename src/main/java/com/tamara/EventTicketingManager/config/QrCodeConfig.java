b30538e5ec972d9dd809cf39123959623d2286e2
tamaraghosn <tamaraghosn01@gmail.com>
2025-12-08 13:11:53 +0200

    added the zxing dependency for QRcode generating and implemented a method to generate the QrCode in the service layer

---
+++ src/main/java/com/tamara/EventTicketingManager/config/QrCodeConfig.java
@@ -0,0 +1,15 @@
+package com.tamara.EventTicketingManager.config;
+
+
+import com.google.zxing.qrcode.QRCodeWriter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class QrCodeConfig {
+
+    @Bean
+    public QRCodeWriter QrCodeWriter(){
+        return new QRCodeWriter();
+    }
+}
